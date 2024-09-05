926c58409e5fe049679ba78dd520728f13e52318
Almarch <alexandre.marchal@hotmail.com>
2024-09-05 13:44:33 +0200

    try fix connect as admin

--- config/ssl.sh
+++ config/ssl.sh
@@ -1,9 +1,8 @@
-# https://gist.github.com/taoyuan/39d9bc24bafc8cc45663683eae36eb1a
 
-openssl genrsa -out cert.key 2048
-openssl req -new -key cert.key -out cert.csr
-openssl x509 -req -days 3650 -in cert.csr -signkey cert.key -out cert.crt
+# ====== valid certificat
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d lycaste.eu -d www.lycaste.eu
 
-sudo mkdir /etc/nginx/ssl
-sudo mv cert.crt /etc/nginx/ssl/lycaste.eu.crt
-sudo mv cert.key /etc/nginx/ssl/lycaste.eu.key
+
+
+
