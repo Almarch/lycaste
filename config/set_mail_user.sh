45cb24c19b8b529404b80414729fd39d6f5c90d3
Almarch <alexandre.marchal@hotmail.com>
2024-09-20 15:05:03 +0200

    email access

--- config/set_mail_user.sh
+++ config/set_mail_user.sh
@@ -1,3 +1,3 @@
 # from within the smtp container
 
-saslpasswd2 -c -u wonderfulDomain.com "admin" <<< "qwerty" # use actual password instead
+saslpasswd2 -c -u lycaste.eu "admin" <<< "qwerty" # use actual password instead
