87346dab61a792085d1df0f98279849813cdfc7d
Almarch <alexandre.marchal@hotmail.com>
2024-09-23 08:27:19 +0200

    use related keys

--- lycaste/main/models.py
+++ lycaste/main/models.py
@@ -8,7 +8,7 @@ class Taxon(models.Model):
         return f'{self.name}'
 
     name  = models.fields.CharField(null=False, max_length=100)
-    supra = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='offspring')
+    supra = models.ForeignKey('self', related_name='infra', null=True, blank=True, on_delete=models.SET_NULL)
     photo = models.fields.CharField(null=True)
     description_fr = models.fields.CharField(null=True)
     description_es = models.fields.CharField(null=True)
@@ -16,7 +16,7 @@ class Taxon(models.Model):
     
 class Genotype(models.Model):
     name = models.fields.CharField(null=True, max_length=100)
-    cross = models.ForeignKey('Cross', null=True, on_delete=models.SET_NULL)
+    cross = models.ForeignKey('Cross', related_name='siblings', null=True, on_delete=models.SET_NULL)
     location = models.fields.CharField(null=True, max_length=255)
     photo = models.fields.CharField(null=True)
     description_fr = models.fields.CharField(null=True)
@@ -24,20 +24,20 @@ class Genotype(models.Model):
     description_en = models.fields.CharField(null=True)
 
 class Cross(models.Model):
-    mother = models.ForeignKey(Genotype, null=True, on_delete=models.SET_NULL)
-    father = models.ForeignKey(Genotype, null=True, on_delete=models.SET_NULL)
+    mother = models.ForeignKey(Genotype, related_name='offspring_as_mother', null=True, on_delete=models.SET_NULL)
+    father = models.ForeignKey(Genotype, related_name='offspring_as_father', null=True, on_delete=models.SET_NULL)
     who_crossed  = models.fields.CharField(null=True, max_length=255)
     date_crossed = models.fields.DateField(null=True)
     date_sown    = models.fields.DateField(null=True)
 
 class Event(models.Model):
-    name = models.fields.CharField(null=False, max_length=100)
-    date = models.fields.DateField(null=False)
-    country = models.fields.CharField(null=False, max_length=50)
-    city = models.fields.CharField(null=False, max_length=50)
+    name        = models.fields.CharField(null=False, max_length=100)
+    date        = models.fields.DateField(null=False)
+    country     = models.fields.CharField(null=False, max_length=50)
+    city        = models.fields.CharField(null=False, max_length=50)
     description = models.fields.CharField(null=True, max_length=255)
 
 class Distinction(models.Model):
-    genotype = models.ForeignKey(Genotype, null=True, on_delete=models.SET_NULL)
-    event = models.ForeignKey(Event, null=True, on_delete=models.SET_NULL)
+    genotype = models.ForeignKey(Genotype, related_name='all_prices', null=True, on_delete=models.SET_NULL)
+    event    = models.ForeignKey(Event,    related_name='all_prices', null=True, on_delete=models.SET_NULL)
     description = models.fields.CharField(null=True, max_length=255)
